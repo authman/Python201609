728f7df506be5a4de9d9cf68c15eb942177c6897
kenkoolio <kensoko0l@yahoo.com>
2016-09-02 15:23:51 -0500

    committed multiples and sumList assignment

---
+++ Nguyen_Ken/Assignments/multiples.py
@@ -0,0 +1,10 @@
+#multiples assignment part 1
+for count in range(1,1001):
+    print count
+    count+=2
+
+
+#multiples assignment part 2
+for count in range(5, 1000001):
+    print count
+    count+=5
