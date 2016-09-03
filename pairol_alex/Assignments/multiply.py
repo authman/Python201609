6b483e1515ae732651ecc49ebcc77a8f80fceee6
Alex Pairol <pairolalex@gmail.com>
2016-09-03 16:07:00 -0500

    N/A

---
+++ pairol_alex/Assignments/multiply.py
@@ -0,0 +1,3 @@
+originalList = [1, 2, 3, 4, 5]
+newList = [item*5 for item in originalList]
+print(newList)
---
+++ pairol_alex/Assignments/oddAndEvens.py
@@ -0,0 +1,5 @@
+for x in range(0, 10):
+    if  x % 2 !=0 :
+        print ("Number is " +  str(x) + ".This is an odd")
+    else:
+        print ("Number is " +  str(x) + ".This is an even")
