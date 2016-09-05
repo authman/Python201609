af29727b5356d5aa0ef657fc3b6fdf7bc6a44e52
kenkoolio <kensoko0l@yahoo.com>
2016-09-05 18:16:37 -0500

    changed variable name from "count" to "pointer", hoping it would be more descriptive when revisted

--- Nguyen_Ken/Assignments/insertionSort.py
+++ Nguyen_Ken/Assignments/insertionSort.py
@@ -3,16 +3,16 @@
 list = [2,4,8,9,55,61,22,8,80,91,100,-100,79,14,31,13,3,7]
 
 for i in range(len(list)):
-    count = 0
-    while count < i:
-        if list[i]<list[count]:
+    pointer = 0
+    while pointer < i:
+        if list[i]<list[pointer]:
             temp=list[i]
-            for j in range(i,count,-1):
+            for j in range(i,pointer,-1):
                 list[j]=list[j-1]
-            list[count]=temp
+            list[pointer]=temp
 
         else:
-            count += 1
+            pointer += 1
 
 
 print list
