8470f2104c15137ad0f2fcfa92a3bbe528ae5146
Will Wright <wawwright@gmail.com>
2016-09-07 15:42:38 -0500

    It works! ready to submit

--- Wright_Will/Assignments/ninja_gold/server.py
+++ Wright_Will/Assignments/ninja_gold/server.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request, redirect,session
+from flask import Flask, render_template, request, redirect,session,Markup
 from random import randint
+from datetime import datetime, date, time
 app = Flask(__name__)
-app.secret_key = "fred"
+app.secret_key = "Dan"
 
 @app.route('/',methods=['GET','POST'])
 def index():
     if not session.has_key('log'):
-        session['log'] = ""
+        session['log'] = []
     if not session.has_key('gold'):
         session['gold'] = 0
-    return render_template("index.html")
+    return render_template("index.html",gold=session['gold'],log=session['log'])
 @app.route('/processgold',methods=['GET','POST'])
 def processgold():
     earnings = 0
@@ -22,7 +23,22 @@ def processgold():
     elif  request.form['place'] == 'casino':
         earnings = randint(-50,51)
     session['gold'] += earnings
-    session['log'] += "\n" + "Earned " + str(earnings) + "gold from the " + request.form['place'] + "!"
-    print session['log']
+
+    html_string = "<h6 "
+    if earnings < 0:
+        html_string += "class = 'red'"
+    html_string +=  ">"
+    if earnings < 0:
+        html_string += "Lost"
+    else:
+        html_string += "Earned"
+    html_string +=  str(earnings) + " gold from the "
+    html_string +=  request.form['place'] + "! "
+    html_string +=  str(datetime.now().strftime("(%Y/%m/%d  %H:%M)"))
+    html_string +=  "</h6>"
+    print html_string
+    if(len(session['log']) > 12):
+        session['log'] = session['log'][1:len(session['log'])]
+    session['log'].append(html_string)
     return redirect('/')
 app.run(debug=True)
