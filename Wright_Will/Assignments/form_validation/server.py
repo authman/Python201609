4ab59f78a07bb6acff1f8dc020364f1192fa20e9
Will Wright <wawwright@gmail.com>
2016-09-07 16:26:19 -0500

    copy of dojo_survery made for formvalidation

--- Wright_Will/Assignments/form_validation/server.py
+++ Wright_Will/Assignments/form_validation/server.py
@@ -1,5 +1,5 @@
 #server
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request,redirect,flash
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,17 @@ def results():
     location = request.form['location']
     language = request.form['language']
     comment =request.form['comment']
+    if len(name) < 1:
+        flash('name must not be blank')
+        #return redirect('/')
+    if len(comment) < 1:
+        flash('comment must not be blank')
+        #return redirect('/')
+    if len(comment) > 120:
+        flash('comment can not be larger than 120 charachters')
+        #return redirect('/')
+
     return render_template("results.html",name=name,location=location,language=language,comment=comment)
+@app.route('/process', methods=['POST'])
 
 app.run(debug=True)
