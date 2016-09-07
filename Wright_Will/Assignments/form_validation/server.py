26a7c94fd486753eb3c91ec2f426d4e4e9df7927
Will Wright <wawwright@gmail.com>
2016-09-07 17:35:57 -0500

    everything works!

--- Wright_Will/Assignments/form_validation/server.py
+++ Wright_Will/Assignments/form_validation/server.py
@@ -1,28 +1,38 @@
 #server
-from flask import Flask, render_template, request,redirect,flash
+from flask import Flask, render_template, request,redirect,flash,session
 app = Flask(__name__)
+app.secret_key = "bob"
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+
+    if not session.has_key('name'):
+        session['name'] = ""
+    if not session.has_key('location'):
+        session['location'] = "San Fransisco"
+    if not session.has_key('language'):
+        session['language'] = "Python"
+    if not session.has_key('comment'):
+        session['comment'] = ""
+    return render_template("index.html",name=session['name'],location=session['location'],language=session['language'],comment=session['comment'])
 
 @app.route('/results',methods = ["post"])
 def results():
-    name =request.form['name']
-    location = request.form['location']
-    language = request.form['language']
-    comment =request.form['comment']
-    if len(name) < 1:
+    session['name'] =request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comment'] =request.form['comment']
+    if len(session['name']) < 1:
         flash('name must not be blank')
-        #return redirect('/')
-    if len(comment) < 1:
+        return redirect('/')
+    if len(session['comment']) < 1:
         flash('comment must not be blank')
-        #return redirect('/')
-    if len(comment) > 120:
+        return redirect('/')
+    if len(session['comment']) > 120:
         flash('comment can not be larger than 120 charachters')
-        #return redirect('/')
+        return redirect('/')
 
-    return render_template("results.html",name=name,location=location,language=language,comment=comment)
-@app.route('/process', methods=['POST'])
+    return render_template("results.html",name=session['name'],location=session['location'],language=session['language'],comment=session['comment'])
+
 
 app.run(debug=True)
