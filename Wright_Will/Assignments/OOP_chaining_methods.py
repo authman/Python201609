db4c931c325e5205d78171c6000bf6dae54d5d96
Will Wright <wawwright@gmail.com>
2016-09-08 10:35:38 -0500

    added chaining methods assignment

---
+++ Wright_Will/Assignments/OOP_chaining_methods.py
@@ -0,0 +1,35 @@
+#bike
+
+class Bike(object):
+    def __init__(self,price,max_speed):
+        self.price = price
+        self.max_speed = max_speed
+        self.miles = 0
+    def displayInfo(self):
+        print "price: " + str(self.price)
+        print "max_speed: " + str(self.max_speed)
+        print "miles: " + str(self.miles)
+        return self
+    def ride(self):
+        print 'riding'
+        self.miles += 10
+        return self
+    def reverse(self):
+        print 'reversing'
+        if self.miles < 5:
+            self.miles = 0
+        else:
+            self.miles -= 5
+        return self
+
+
+a = Bike(100,10)
+b = Bike(10,1)
+c = Bike(50,5)
+
+print "bike a:"
+a.ride().ride().ride().reverse().displayInfo()
+print "bike b:"
+b.ride().ride().reverse().reverse().displayInfo()
+print "bike c:"
+c.reverse().reverse().displayInfo()
