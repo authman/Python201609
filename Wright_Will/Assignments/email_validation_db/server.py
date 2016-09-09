7499a47a2ddf8aa28e6e285381342924b95114e0
Will Wright <wawwright@gmail.com>
2016-09-09 07:01:12 -0500

    server up and running

--- Wright_Will/Assignments/email_validation_db/server.py
+++ Wright_Will/Assignments/email_validation_db/server.py
@@ -1,19 +1,30 @@
-from flask import Flask, render_template,redirect
-from mysqlconnection import MySQLconnector
+from flask import Flask, render_template,redirect,request,session
+from mysqlconnection import MySQLConnector
+import re
 app = Flask(__name__)
-mysql = MySQLconnector(app,'email_validation')
+app.secret_key = "fred"
+mysql = MySQLConnector(app,'email_validation')
 
 @app.route('/')
 def home():
-
-    return render_template('index.html')
-@app.route('/results')
+    if not 'email' in session:
+        session['email'] = ""
+    if not 'name' in session:
+        session['name'] = ""
+    return render_template('index.html',name=session['name'],email=session['email'])
+@app.route('/results' )
 def results():
     return render_template('results.html')
-@app.route('/addUser'method='POST')
+@app.route('/addUser',methods=['POST'])
 def addUser():
+    session["name"] = request.form["name"]
+    session["email"] = request.form["email"]
+    #if not re.match(session["email"],r"[a-Z0-9_]@.*\..*"):
+    if len(session["name"]) < 1 or len(session["email"]) < 1:
+        return redirect("/")
+
     return redirect('/results')
-@app.route('/deleteUser'method='POST')
+@app.route('/deleteUser',methods=['POST'])
 def deleteUser():
 
     return redirect('/results')
