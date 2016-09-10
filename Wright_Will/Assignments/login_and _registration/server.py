defdcf8a04cedc57dcf42ad90e51b70c70737140
Will Wright <wawwright@gmail.com>
2016-09-10 15:51:58 -0500

    styling done basic validation added

--- Wright_Will/Assignments/login_and _registration/server.py
+++ Wright_Will/Assignments/login_and _registration/server.py
@@ -1,15 +1,33 @@
-from flask import Flask, session, redirect, render_template, flash
+from flask import Flask, session, redirect, render_template, flash, request
 from mysqlconnection import MySQLConnector
 import re
 app = Flask(__name__)
-# mysql = MySQLConnector(app,db)
+app.secret_key = 'fred'
+mysql = MySQLConnector(app,'login_reg')
 
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/process')
+@app.route('/process',methods=['POST'])
 def process():
-    pass
+    email = request.form['email']
+    fname = request.form['fname']
+    lname = request.form['lname']
+    password = request.form['password']
+    confirm_pass = request.form['confirm_pass']
+    error = False
+
+    if not re.match(r"\b[a-z]+[a-z0-9_]*@.+\..+\b",email,):
+        flash("not a valid email")
+        error = True
+
+    if not re.match(r"\b[a-zA-Z]+\b",fname,):
+        flash("First Name can only contain letters and must be at least 2 charachters")
+        error = True
+
+    if error:
+        return redirect('/')
+    return redirect('/success')
 @app.route('/success')
 def success():
     return render_template('success.html')
