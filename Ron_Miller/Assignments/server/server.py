8d5ef0dd327ce12dafcefc904dd2667996596b6a
Ron Miller <rkstmiller@gmail.com>
2016-09-13 16:47:33 -0500

    Monthly commit

---
+++ Ron_Miller/Assignments/server/server.py
@@ -0,0 +1,18 @@
+from flask import Flask ,render_template, request, redirect
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/users',methods=['POST'])
+def createUser():
+	print ("Got post info.")
+
+	name = request.form['name']
+	email = request.form['email']
+
+	return redirect('/')
+
+app.run(debug=True)
