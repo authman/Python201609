0fb4473a9e400a6d21fc7bc9d32fa4fe850cb62a
Will Wright <wawwright@gmail.com>
2016-09-16 11:00:47 -0500

    added errror validation

--- Wright_Will/Assignments/weather_api/app.js
+++ Wright_Will/Assignments/weather_api/app.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
 
     $('form').submit(function(){
-        //build url
+        var appid = "1086e6ef833eda7246bfe1fa90897b94"
         var city = $("form [type='text']").val().split(" ").join("%20")
         url = "http://api.openweathermap.org/data/2.5/weather?q=" + city
         url += "&APPID=" + appid
-
         $.get(url, function(res) {
-            // parse data from res JSON object
-            description = res['weather'][0]['description']
-            degK = res['main']['temp']
-            degF = degK * (9/5) - 459.67 // convert to farenheight
-            degF = Math.round(degF)
-            //update page
+            console.log(res)
+            if (res['message']){
+                description =res['message']
+                temp=""
+            }else{
+                description = res['weather'][0]['description']
+                temp = res['main']['temp']
+                temp = temp * (9/5) - 459.67
+                temp = Math.round(temp)
+            }
+
             $( "#description" ).text(description);
-            $( "#temp" ).text(degF);
+            $( "#temp" ).text(temp);
+
         }, 'json');
-        
+
+
         return false
     })
 })
