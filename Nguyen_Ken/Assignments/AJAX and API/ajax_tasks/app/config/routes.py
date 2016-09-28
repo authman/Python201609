31e1af5104aa53c1a9c429812b34405ef6dd0d5d
kenkoolio <kensoko0l@yahoo.com>
2016-09-28 16:27:10 -0500

    updated ajax_task assignment -- that was a tricky one, thanks for the help

---
+++ Nguyen_Ken/Assignments/AJAX and API/ajax_tasks/app/config/routes.py
@@ -0,0 +1,8 @@
+
+from system.core.router import routes
+
+routes['default_controller'] = 'Tasks'
+routes['/'] = 'Tasks#index'
+routes['POST']['/tasks/add'] = 'Tasks#add'
+routes['POST']['/tasks/edit'] = 'Tasks#edit'
+routes['/loadForm'] = 'Tasks#loadForm'
